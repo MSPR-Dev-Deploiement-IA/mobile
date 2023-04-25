5182d9ffe611a4b673cbd69008d9a4e59bea3232
Marc-Olivier-CAS <marcolivier_cas@yahoo.fr>
2023-04-25 15:35:04 +0200

    Add Photo feature.

--- app/src/main/java/fr/epsi/arosaje/MainActivity.kt
+++ app/src/main/java/fr/epsi/arosaje/MainActivity.kt
@@ -1,11 +1,17 @@
 package fr.epsi.arosaje
 
+import android.Manifest
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,6 +32,19 @@ class MainActivity : AppCompatActivity() {
         }
 
         photoButton.setOnClickListener {
+            // Vérifier si la permission pour utiliser la caméra est accordée
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+                // Demander la permission pour utiliser la caméra
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.CAMERA), MY_PERMISSIONS_REQUEST_CAMERA)
+            } else {
+                // Lancer l'application de la caméra
+                val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                if (takePictureIntent.resolveActivity(packageManager) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
         }
 
         conseilsButton.setOnClickListener {
@@ -38,5 +57,28 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, ProfileActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    // Cette fonction est appelée lorsque l'utilisateur répond à la demande de permission
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_CAMERA -> {
+                // Si la permission est accordée, lancer l'application de la caméra
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    if (takePictureIntent.resolveActivity(packageManager) != null) {
+                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                    }
+                }
+                return
+            }
+        }
+    }
+
+    companion object {
+        const val REQUEST_IMAGE_CAPTURE = 1
+        const val MY_PERMISSIONS_REQUEST_CAMERA = 2
     }
 }
