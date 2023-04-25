08875f5c0f9e4a416f01f9f6134873a06c27da65
Marc-Olivier-CAS <marcolivier_cas@yahoo.fr>
2023-04-25 15:12:20 +0200

    Save, working on main screen

--- app/src/main/java/fr/epsi/arosaje/MainActivity.kt
+++ app/src/main/java/fr/epsi/arosaje/MainActivity.kt
@@ -10,32 +10,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Récupération des boutons depuis la vue
+        val deconnexionButton: Button = findViewById(R.id.deconnexion_button)
+        deconnexionButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
         val carteButton: Button = findViewById(R.id.carte_button)
         val photoButton: Button = findViewById(R.id.photo_button)
         val conseilsButton: Button = findViewById(R.id.conseils_button)
         val chatButton: Button = findViewById(R.id.chat_button)
         val profilButton: Button = findViewById(R.id.profil_button)
 
-        // Ajout des listeners pour chaque bouton
         carteButton.setOnClickListener {
-            // Action à effectuer lors du clic sur le bouton "Carte"
         }
 
         photoButton.setOnClickListener {
-            // Action à effectuer lors du clic sur le bouton "Photo"
         }
 
         conseilsButton.setOnClickListener {
-            // Action à effectuer lors du clic sur le bouton "Conseils"
         }
 
         chatButton.setOnClickListener {
-            // Action à effectuer lors du clic sur le bouton "Chat"
         }
 
         profilButton.setOnClickListener {
-            // Redirection vers l'activité ProfileActivity
             val intent = Intent(this, ProfileActivity::class.java)
             startActivity(intent)
         }
