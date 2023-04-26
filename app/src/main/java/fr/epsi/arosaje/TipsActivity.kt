e2da1ccca2ac01c3e4101e0f099105bf70edeead
Marc-Olivier-CAS <marcolivier_cas@yahoo.fr>
2023-04-26 16:19:29 +0200

    Save

--- app/src/main/java/fr/epsi/arosaje/TipsActivity.kt
+++ app/src/main/java/fr/epsi/arosaje/TipsActivity.kt
@@ -2,6 +2,7 @@ package fr.epsi.arosaje
 
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +12,15 @@ class TipsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tips)
+
+        // Récupération du header
+        val header: View = findViewById(R.id.header_layout)
+        val backButton: ImageView = header.findViewById(R.id.back_button)
+        val titleTextView: TextView = header.findViewById(R.id.title_textview)
+
+        // Définition du titre et du listener pour le bouton de retour
+        titleTextView.text = "Conseils"
+        backButton.setOnClickListener { finish() }
 
         // Initialise le texte centré
         val tipsTextView = findViewById<TextView>(R.id.tips_text)
