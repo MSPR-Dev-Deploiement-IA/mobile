9d2b8a351d9a76640df398424ff87318c3f56e64
Marc-Olivier-CAS <marcolivier_cas@yahoo.fr>
2023-04-26 16:22:09 +0200

    Save

--- app/src/main/java/fr/epsi/arosaje/ProfileActivity.kt
+++ app/src/main/java/fr/epsi/arosaje/ProfileActivity.kt
@@ -14,37 +14,32 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        // Récupération du header
         val header: View = findViewById(R.id.header_layout)
         val backButton: ImageView = header.findViewById(R.id.back_button)
         val titleTextView: TextView = header.findViewById(R.id.title_textview)
 
-        // Définition du titre et du listener pour le bouton de retour
         titleTextView.text = "Mon profil"
         backButton.setOnClickListener { finish() }
 
-        // Récupération des boutons depuis la vue
         val photosButton: Button = findViewById(R.id.photos_button)
         val historiqueButton: Button = findViewById(R.id.history_button)
         val favorisButton: Button = findViewById(R.id.favorites_button)
         val modifierProfilButton: Button = findViewById(R.id.edit_profile_button)
 
-        // Ajout des listeners pour chaque bouton
         photosButton.setOnClickListener {
             val intent = Intent(this, GalleryActivity::class.java)
             startActivity(intent)
         }
 
         historiqueButton.setOnClickListener {
-            // Action à effectuer lors du clic sur le bouton "Historique"
         }
 
         favorisButton.setOnClickListener {
-            // Action à effectuer lors du clic sur le bouton "Favoris"
         }
 
         modifierProfilButton.setOnClickListener {
-            // Action à effectuer lors du clic sur le bouton "Modifier Profil"
+            val intent = Intent(this, UpdateprofileActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/fr/epsi/arosaje/UpdateprofileActivity.kt
@@ -0,0 +1,4 @@
+package fr.epsi.arosaje
+
+class UpdateprofileActivity {
+}
