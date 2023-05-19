686581f1290adaadefda68eb4aa071f8d7ee9977
Marc-Olivier-CAS <marcolivier_cas@yahoo.fr>
2023-05-19 15:10:06 +0200

    Login is working

--- app/src/main/java/fr/epsi/arosaje/LoginActivity.kt
+++ app/src/main/java/fr/epsi/arosaje/LoginActivity.kt
@@ -7,53 +7,33 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import okhttp3.*
+import java.io.IOException
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.encodeToJsonElement
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
 
 class LoginActivity : AppCompatActivity() {
-
-    private val userDatabase = mutableMapOf(
-        "marco" to "1234",
-        "germain" to "1234",
-        "thomas" to "1234",
-        "max" to "1234",
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val editTextUsername = findViewById<EditText>(R.id.editTextUsername)
+        val editTextMail = findViewById<EditText>(R.id.editTextEmail)
         val editTextPassword = findViewById<EditText>(R.id.editTextPassword)
         val buttonLogin = findViewById<Button>(R.id.buttonSubmit)
         val textViewNoAccount = findViewById<TextView>(R.id.textViewNoAccount)
 
-        val intentKeys = IntentKeys
-
-        // Récupérer les informations de l'utilisateur ajouté dans la SignActivity
-        val intent = intent
-        val newUsername = intent.getStringExtra(intentKeys.EXTRA_USERNAME)
-        val newPassword = intent.getStringExtra(intentKeys.EXTRA_PASSWORD)
-        if (newUsername != null && newPassword != null) {
-            userDatabase[newUsername] = newPassword
-        }
-
         buttonLogin.setOnClickListener {
-            val username = editTextUsername.text.toString()
+            val email = editTextMail.text.toString()
             val password = editTextPassword.text.toString()
 
-            if (userDatabase.containsKey(username) && userDatabase[username] == password) {
-                // Les informations d'identification sont valides, connectez l'utilisateur
-                Toast.makeText(this, "Connexion réussie", Toast.LENGTH_SHORT).show()
-
-                // Lancer l'activité SplashActivity
-                val intent = Intent(this@LoginActivity, SplashloginActivity::class.java)
-                startActivity(intent)
-
-                /*// Lancer l'activité principale
-                val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                startActivity(intent)*/
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                login(email, password)
             } else {
-                // Les informations d'identification sont invalides, afficher un message d'erreur
-                Toast.makeText(this, "Nom d'utilisateur ou mot de passe incorrect", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Veuillez remplir tous les champs", Toast.LENGTH_SHORT).show()
             }
         }
 
@@ -61,5 +41,49 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this@LoginActivity, SignActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun login(email: String, password: String) {
+        val client = OkHttpClient()
+
+        val json = Json.encodeToJsonElement(
+            JsonObject(mapOf(
+                "email" to Json.encodeToJsonElement(email),
+                "password" to Json.encodeToJsonElement(password)
+            ))
+        ).toString()
+
+        val mediaType = "application/json; charset=utf-8".toMediaType()
+        val requestBody = json.toRequestBody(mediaType)
+
+        val request = Request.Builder()
+            .url("http://10.0.2.2:8080/auth/login")
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                runOnUiThread {
+                    println("1 + $email + $password")
+                    Toast.makeText(applicationContext, "Erreur lors de la connexion", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    runOnUiThread {
+                        Toast.makeText(applicationContext, "Connexion réussie", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(applicationContext, SplashloginActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                } else {
+                    runOnUiThread {
+                        println("2 + $email + $password")
+                        Toast.makeText(applicationContext, "Nom d'utilisateur ou mot de passe incorrect", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        })
     }
 }
